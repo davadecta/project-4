cddd392bea53c004afe08d1b278719b6ad28e83e
davadecta <125406050+davadecta@users.noreply.github.com>
2023-02-15 03:46:22 +0700

    Update Simple REST API.php

--- Simple REST API.php
+++ Simple REST API.php
@@ -1,7 +1,7 @@
 <?php
 
 // Connect to the database
-$dsn = 'mysql:host=localhost;dbname=my_rest';
+$dsn = 'mysql:host=localhost;dbname=yourdbname';
 $username = 'root';
 $password = '';
 $db = new PDO($dsn, $username, $password);
@@ -14,16 +14,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   echo json_encode($data);
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-    <h1>Terima Kasih pak Arief telah menjadi juri untuk LKS Cloud Computing Kota Malang Tahun 2023</h1>
-</body>
-</html>
